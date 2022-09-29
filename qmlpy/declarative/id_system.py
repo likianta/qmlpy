62b11a97874c19b8dc1eaacb51e7bda9f8ab4baa
likianta <likianta@foxmail.com>
2022-09-29 23:17:59 +0800

    Change id to dataclass type

--- qmlpy/declarative/id_system.py
+++ qmlpy/declarative/id_system.py
@@ -1,47 +1,79 @@
+from __future__ import annotations
+
 import typing as t
 from collections import defaultdict
+from dataclasses import dataclass
 
 
 class T:
     Component = ...
     Level = int
-    IdChain = t.Dict[Level, int]
-    Qid = str
-    QidList = t.List[Qid]
+    IdChain = t.Dict[int, int]
+    Token = t.Tuple[int, ...]
+
+
+@dataclass
+class Id:
+    token: T.Token
+    
+    def __str__(self) -> str:
+        return self.text
+    
+    @property
+    def text(self) -> str:
+        return '_'.join(map(str, self.token))
+    
+    @property
+    def level(self) -> int:  # >= 1
+        return len(self.token)
+    
+    @property
+    def number(self) -> int:  # >= 0
+        return self.token[-1]
+    
+    @property
+    def parent(self) -> t.Optional['Id']:
+        if len(self.token) > 1:
+            return Id(tuple(self.token[:-1]))
+        else:
+            return None
+    
+    @property
+    def last_sibling(self) -> t.Optional['Id']:
+        if x := self.token[-1]:
+            return Id((*self.token[:-1], x - 1))
+        else:
+            return None
+    
+    @property
+    def next_sibling(self) -> 'Id':
+        x = self.token[-1]
+        return Id((*self.token[:-1], x + 1))
 
 
 class IdGenerator:
+    level: T.Level
+    _id_chain: T.IdChain
     
-    def __init__(self, prefix='qid_'):
+    def __init__(self):
         self.level = 0
-        self._id_chain = defaultdict(int)  # type: T.IdChain
-        self._prefix = prefix
+        self._id_chain = defaultdict(int)
     
-    def upgrade(self) -> int:
+    def upgrade(self) -> T.Level:
         self.level += 1
         self._id_chain[self.level] += 1
         return self.level
     
-    def downgrade(self) -> int:
+    def downgrade(self) -> T.Level:
         # reset children level count
         for level in range(self.level + 1, max(self._id_chain)):
             self._id_chain.pop(level)
         self.level -= 1  # go to uplevel
         return self.level
     
-    def gen_id(self) -> str:
-        return self._prefix + '_'.join(
-            hex(self._id_chain[level])
-            for level in range(1, self.level + 1)
-        )
-    
-    @property
-    def root_id(self):
-        return self._prefix + '0x1'
-
-
-def get_id_level(qid: T.Qid):
-    return qid.count('_') - 1
+    def gen_id(self) -> Id:
+        # note: the keys in self._id_chain are ordered by level sequence.
+        return Id(tuple(self._id_chain.values()))
 
 
 class IdManager:
