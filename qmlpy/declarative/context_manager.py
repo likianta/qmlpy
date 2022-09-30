217797a77c09a408d32063394e5c9b8da81359b0
likianta <likianta@foxmail.com>
2022-09-30 14:09:56 +0800

    Simplify context manager

--- qmlpy/declarative/context_manager.py
+++ qmlpy/declarative/context_manager.py
@@ -1,63 +1,20 @@
+from .id_system import T
 from .id_system import id_gen
-from .black_magic import this
+from .id_system import id_mgr
 
-
-class T:
-    Component = ...
+__all__ = ['ctx_mgr']
 
 
 class ContextManager:
     
-    def __init__(self):
-        self._id_gen = id_gen
-        self._root = []
-        self._node = self._root
-        self._node_history = []
+    @staticmethod
+    def enter(comp: T.Component) -> None:
+        id_ = id_gen.upgrade()
+        id_mgr.register(id_, comp)
     
-    def lift(self, comp: T.Component):
-        this._pointer = comp
-        
-        temp = []
-        self._node.append((comp, temp))
-        last_node, self._node = self._node, temp
-        self._node_history.append(last_node)
-        
-        ''' Illustration
-        
-        step 1:
-            [(A, [])] -> [(A, [(B, [])])]
-            |    ^^1|    |    | ^2 ^^3| |
-            ^-4-----^    |    ^-1-----^ |
-                         ^-4------------^
-            1: self._node
-            2: obj
-            3: temp
-            4: self._root
-            ~. self._node_history = [#4]
-        
-        step 2:
-            [(A, [(B, [])])]
-            |    |    ^^1| |
-            |    ^-2-----^ |
-            ^-3------------^
-            1. self._node = temp
-            3. self._root
-            ~. self._node_history = [#3, #2]
-        '''
-
-        self._id_gen.upgrade()
-        return self._id_gen.gen_id()
-    
-    def drop(self):
-        self._node = self._node_history.pop()
-        self._id_gen.downgrade()
-    
-    def dump(self):
-        return self._root
-    
-    @property
-    def level(self):
-        return self._id_gen.level
+    @staticmethod
+    def leave() -> None:
+        id_gen.downgrade()
 
 
 ctx_mgr = ContextManager()
