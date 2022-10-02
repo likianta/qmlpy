1610eb4063afca24e5a3105ffa6a13a69e5f30a5
likianta <likianta@foxmail.com>
2022-10-02 20:26:15 +0800

    Refactor blueprint io

--- blueprint/readme.md
+++ blueprint/readme.md
@@ -6,7 +6,7 @@ For some history reasons, "blueprint" remains the old-style code, which is not
 much compilant with current one.
 
 I'm working on to update this project to follow the new guidelines. But before
-this work is done, be notice that there are some inconvinience listed below:
+this work is done, be notice that there are some inconviniences listed below:
 
 - Most comments were written in Chinese.
 - Its parser was focus on parsing Qt 5.15, which is slightly incompatible with
@@ -16,6 +16,8 @@ this work is done, be notice that there are some inconvinience listed below:
 
 # Requirements
 
+## Python
+
 Required python 3.8+ (recommend 3.10) and pip install...
 
 - argsense
@@ -23,22 +25,37 @@ Required python 3.8+ (recommend 3.10) and pip install...
 - lk-logger >= 5.4.0
 - lk-utils >= 2.4.0
 
-Resources preparation see `./src/qml_modules_indexing/io.py:docstring`.
+## Qt Documents
 
-# How to use
+1. Install official Qt 5.15 or Qt 6.x.
+    1. Assumed you haved installed in `d:/programs/qt` (laterly we call it
+    `<qt_root>`)
+2. Copy `<qt_root>/Docs/Qt-5.15/qtdoc/modules-qml.html` to
+   `<project>/blueprint/resources/qtdoc/1_all_qml_modules.html`.
+3. Copy `<qt_root>/Docs/Qt-5.15/qtdoc/qmltypes.html` to
+   `<project>/blueprint/resources/qtdoc/2_all_qml_types.html`.
+4. Edit `<project>/blueprint/src/io.py`, change the value `qt_source = None` to
+   the path to your "qtdoc" path, for example:
+   `qt_source = 'd:/programs/qt/Docs/Qt-5.15/qtdoc'`.
 
-To generate structured data from `resources/*.html` to `resouces/*.json`, run
-this script:
+# Run
 
 ```sh
-cd src/qml_modules_indexing
-python main.py
-```
+cd blueprint
 
-To generate `qmlpy` modules from `resouces/*.json` to `qmlpy/widgets/*`, run
-this script:
+# Get help
+python run.py -h
 
-```sh
-cd src/template_generator
-python main.py
+# Parse html files and generate json files.
+python run.py indexing-qml-modules
+#   (remember adding '-h' can also get help.)
+
+# Generate basic properties
+python run.py generate-basic-properties
+
+# Generate group properties
+python run.py generate-group-properties
+
+# Generate widgets api
+python run.py generate-widgets-api
 ```
--- blueprint/src/io.py
+++ blueprint/src/io.py
@@ -1,44 +1,107 @@
-"""
-# Preparation
+from __future__ import annotations
 
-1. Install Qt application (Qt 6 is recommended).
-2. Edit variable `qtdoc_dir` path to be the qt path in your computer:
-        qtdoc_dir = `<qt>/Docs/Qt-<your_qt_version>`
-   For example:
-        qtdoc_dir = 'E:/programs/qt/qt6/Docs/Qt-6.1.3'
-3. Copy `<qtdoc_dir>/qtdoc/modules-qml.html` to `<resources_dir>/qtdoc/1_all_qml
-   _modules.html`.
-4. Copy `<qtdoc_dir>/qtdoc/qmltypes.html` to `<resources_dir>/qtdoc/2_all_qml
-   _types.html`.
-5. Finished. Now you can start running `./main.py`.
-"""
-from os.path import dirname
-from os.path import abspath
+import typing as t
 
-current_dir = dirname(__file__)
-project_dir = abspath(current_dir + '/../..').replace('\\', '/')
+from lk_utils import relpath
 
-# editable (change this to your qt path in your computer.)
-qtdoc_dir = 'E:/programs/qt/qt6/Docs/Qt-6.1.3'
-resources_dir = f'{project_dir}/blueprint/resources'
-widgets_dir = f'{project_dir}/qmlpy/widgets/api'
 
-html_1 = f'{resources_dir}/qtdoc/1_all_qml_modules.html'
-html_2 = f'{resources_dir}/qtdoc/2_all_qml_types.html'
+class path:  # noqa
+    proj_root = relpath('../..')
+    blueprint = f'{proj_root}/blueprint'
+    resources = f'{blueprint}/blueprint/resources'
+    qt_source = None  # TODO: set your own qt source path
+    
+    html_1 = f'{resources}/qtdoc/1_all_qml_modules.html'
+    html_2 = f'{resources}/qtdoc/2_all_qml_types.html'
+    
+    json_1 = f'{resources}/qtdoc_compiled/1_all_qml_modules.json'
+    json_2 = f'{resources}/qtdoc_compiled/2_all_qml_types.json'
+    json_3 = f'{resources}/qtdoc_compiled/3_all_qml_widgets.json'
+    json_4 = f'{resources}/qtdoc_compiled/4_all_qmlpy_widgets.json'
+    
+    temp_1 = f'{resources}/widgets_template/__base__.py'
+    temp_2 = f'{resources}/widgets_template/__init__.py'
+    temp_3 = f'{resources}/widgets_template/__list__.py'
+    temp_4 = f'{resources}/widgets_template/readme.md'
+    temp_5 = f'{resources}/widgets_template/api_init.py'
+    
+    prop_1 = f'{resources}/properties/basic_types.json'
+    prop_2 = f'{resources}/properties/group_types.json'
+    
+    @property
+    def step_1(self) -> tuple[str, str]:
+        return self.html_1, self.json_1
+    
+    @property
+    def step_2(self) -> tuple[str, str]:
+        return self.html_2, self.json_2
+    
+    @property
+    def step_3(self) -> tuple[str, str]:
+        return self.json_2, self.json_3
+    
+    @property
+    def step_4(self) -> tuple[str, str]:
+        return self.json_3, self.json_4
 
-json_1 = f'{resources_dir}/qtdoc_compiled/1_all_qml_modules.json'
-json_2 = f'{resources_dir}/qtdoc_compiled/2_all_qml_types.json'
-json_3 = f'{resources_dir}/qtdoc_compiled/3_all_qml_widgets.json'
-json_4 = f'{resources_dir}/qtdoc_compiled/4_all_pyml_widgets.json'
 
-module_1 = f'{resources_dir}/widgets_template/__base__.py'
-module_2 = f'{resources_dir}/widgets_template/__init__.py'
-module_3 = f'{resources_dir}/widgets_template/__list__.py'
-
-readme = f'{resources_dir}/widgets_template/readme.md'
-api_init = f'{resources_dir}/widgets_template/api_init.txt'
-
-no1 = (html_1, json_1)
-no2 = (html_2, json_2)
-no3 = (json_2, json_3)
-no4 = (json_3, json_4)
+class T:
+    JsonData1 = t.TypedDict('JsonData1', {
+        'module_group': t.Dict[str, str],
+        'module': t.Dict[str, str],
+    })
+    ''' str#1. module name in all lower case, e.g. 'qtquick'.
+        str#2: module name, e.g. 'QtQuick'.
+        str#3: sub module name in all lower case, e.g. 'qtquick-controls'.
+        str#4: sub module name, e.g. 'QtQuick.Controls'.
+    '''
+    
+    JsonData2 = t.Dict[str, t.Dict[str, str]]
+    ''' str#1: module name in PascalCase. e.g. 'QtQuick'
+        str#2: qmltype name in PascalCase. e.g. 'QtQuick.Layouts'
+        str#3: a relative path to a html file. the path starts from `qtdoc_dir`.
+            e.g. 'qtcharts/qml-qtcharts-abstractaxis.html'.
+    '''
+    
+    JsonData3 = t.Dict[str, t.Dict[str, t.TypedDict('WidgetInfo', {
+        'parent': t.List[str],
+        'props' : t.Dict[str, str],
+    })]]
+    ''' e.g. {
+            'QtQuick': {
+                'Rectangle': {
+                    'parent': ['QtQuick', 'Item'],
+                    'props': {
+                        'border': 'group',
+                        'border.color': 'color', ...
+                    }
+                }, ...
+            }, ...
+        }
+    '''
+    
+    JsonData4 = t.Dict[str, t.Dict[str, t.TypedDict('WidgetInfo', {
+        'parent': str,
+        'props' : t.Dict[str, str],
+    })]]
+    ''' e.g. {
+            'qtquick.controls': {
+                'Button': {
+                    'parent': 'AbstractButton',
+                    'props': {
+                        'flat': 'bool',
+                        'highlighted': 'bool', ...
+                    }
+                }, ...
+            },
+            'qtquick': {
+                'Rectangle': {
+                    'parent': 'Item',
+                    'props': {
+                        'antialiasing': 'bool',
+                        'border': 'group', ...
+                    }
+                }, ...
+            }, ...
+        }
+    '''
